8aa33387ab90a297f0fc14444e723cbd9912ad47
Andreea Ciobota <135983461+Andreeaciobota@users.noreply.github.com>
2023-09-13 19:06:57 +0200

    [Add]:background and container

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const itemsWrapper = document.querySelector('.items-wrapper');
+const circlesWrapper = document.querySelector('.circles');
+const btnNext = document.querySelector('.right');
+const btnPrev = document.querySelector('.left');
+
+btnPrev.classList.add('hide');
+
+const images = [
+ "img/01.webp", 
+"img/02.webp" ,
+"img/03.webp" ,
+"img/04.webp" ,
+"img/05.webp"
+];
